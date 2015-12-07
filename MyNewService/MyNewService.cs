fc672df54a402a2e78f80de912c99aa041cf4cc2
rick <wyliuxin0125@163.com>
2015-12-07 11:14:22 +0800

    commit

---
+++ MyNewService/MyNewService.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace MyNewService
+{
+    /// <summary>
+    /// 服务状态
+    /// </summary>
+    public enum ServiceState
+    {
+        SERVICE_STOPPED = 0x00000001,
+        SERVICE_START_PENDING = 0x00000002,
+        SERVICE_STOP_PENDING = 0x00000003,
+        SERVICE_RUNNING = 0x00000004,
+        SERVICE_CONTINUE_PENDING = 0x00000005,
+        SERVICE_PAUSE_PENDING = 0x00000006,
+        SERVICE_PAUSED = 0x00000007,
+    }
+
+    /// <summary>
+    /// 状态结构
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ServiceStatus
+    {
+        public long dwServiceType;
+        public ServiceState dwCurrentState;
+        public long dwControlsAccepted;
+        public long dwWin32ExitCode;
+        public long dwServiceSpecificExitCode;
+        public long dwCheckPoint;
+        public long dwWaitHint;
+    };
+
+    public partial class MyNewService : ServiceBase
+    {
+        #region 构造函数
+
+        public MyNewService()
+        {
+            //Update the service status to Start Pending.
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            //Initialze
+            InitializeComponent();
+            eventLog1 = new EventLog();
+            if (!EventLog.SourceExists("MySource"))
+            {
+                EventLog.CreateEventSource("MySource", "MyNewLog");
+            }
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
+
+            //Update the service status to Running.
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+        #endregion
+
+        #region 服务事件
+
+        //启动事件
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart");
+
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 6000;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In OnStop");
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("In OnContinue");
+        }
+
+        #endregion
+
+        #region 其他事件
+        //定时器事件
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            eventLog1.WriteEntry("Monitoring the System.", EventLogEntryType.Information, eventID++);
+        }
+        #endregion
+
+        #region 其他事件
+        [DllImport("advapi32.dll", SetLastError=true)]
+        private static extern bool SetServiceStatus(IntPtr handle, ref ServiceStatus serviceStatus);
+
+        #endregion
+
+        #region 公共属性
+        public int eventID { get; set; }
+
+        #endregion
+
+    }
+}
